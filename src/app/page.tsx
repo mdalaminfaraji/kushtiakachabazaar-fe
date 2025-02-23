146b440e50dcf4f31546621c2978f072788b5b16
mdalaminfaraji <alaminice1617@gmail.com>
2025-02-23 22:45:45 +0600

    feat: Add theme provider and mode toggle components
    - Introduced theme-provider.tsx for managing theme state.
    - Added modeToggle.tsx for toggling between light and dark modes.
    - Updated layout and page components to support theme changes.
    - Included new dropdown menu component for enhanced UI interactions.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { ModeToggle } from "@/components/common/modeToggle";
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-muted-foreground">
+      Hello world!
+      <ModeToggle />
+    </h1>
+  );
 }
